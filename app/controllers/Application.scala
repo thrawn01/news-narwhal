925ebe6cf9ee5c561f91d1fea596316fbe2fb841
Derrick J. Wippler <thrawn01@gmail.com>
2012-12-07 18:09:47 -0600

    Added static main page

--- app/controllers/Application.scala
+++ app/controllers/Application.scala
@@ -1,12 +1,18 @@
 package controllers
 
-import play.api._
+//import play.api._
 import play.api.mvc._
 
 object Application extends Controller {
   
-  def index = Action {
-    Ok(views.html.index("Your new application is ready."))
+  def index(tag: String) = Action {
+    Ok(views.html.index())
   }
-  
+
+  def latest(tag: String) = TODO
+  def saved(tag: String) = TODO
+  def user(user: String) = TODO
+  def domain(name: String) = TODO
+  def submit = TODO
+
 }
---
+++ app/controllers/Session.scala
@@ -0,0 +1,11 @@
+package controllers
+
+import play.api.mvc._
+
+object Session extends Controller {
+  def preferences = TODO
+  def login = TODO
+  def register = TODO
+  def index = TODO
+  def logout = TODO
+}
